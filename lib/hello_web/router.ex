6e51043ca1a8710455eb95ecce234e5134f21ec4
chewyfungus <collinmaxjohnson@gmail.com>
2018-10-26 10:23:02 -0700

    hello world mvc

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -16,6 +16,7 @@ defmodule HelloWeb.Router do
   scope "/", HelloWeb do
     pipe_through :browser # Use the default browser stack
 
+    get "/hello", HelloController, :index
     get "/", PageController, :index
   end
 
